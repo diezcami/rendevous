97affa7d9dc86015695ed2e030d35280893d6d1f
Basil Begonia <basil.begonia@gmail.com>
2016-05-17 17:27:12 +0800

    added dashboard model and post mode

---
+++ application/models/Post_model.php
@@ -0,0 +1,20 @@
+<?php
+// a model to load the posts.
+class Post_model extends CI_Model {
+	public function __construct()
+	{
+		$this->load->database();
+	}
+	
+	public function record_count() {
+        return $this->db->count_all("post");
+    }
+	
+	// returns posts as an array.
+	public function get_posts($start = 0, $numPosts = 10)
+	{
+		$query = $this->db->get('post', $start, $numPosts);
+		return $query->result_array();
+	}
+}
+?>
